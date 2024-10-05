20537c39485e071ca8523113abfa3268488c6414
Audrea Danskin <73366850+audrealdanskin@users.noreply.github.com>
2024-10-05 15:14:28 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    equinix = {
+      source  = "equinix/equinix"
+      version = "3.0"
+    }
+  }
+}
+
+provider "equinix" {
+  # Configuration options 
+  # Credentials for only Equinix Metal resources 
+  auth_token = ""
+}
+
+#specs of server and machine  
+resource "equinix_metal_device" "web1" {
+  hostname = "audreametal1"
+  plan = "c3.small.x86"
+  metro = "da"
+  operating_system = "ubuntu_20_04"
+  billing_cycle = "hourly"
+  project_id = ""
+}
+
+resource "equinix_metal_vlan" "vlan1" {
+  description = "VLAN in da"
+  metro = "da"
+  project_id = ""
+}
+
+resource "equinix_metal_vlan" "vlan1" {
+  description = "VLAN in SV"
+  metro = "sv"
+  project_id = ""
+}
